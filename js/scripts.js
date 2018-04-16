c42d11c434bab5be2d7a3957dc20efc4d35c9ed1
ivan-ivanovski <38423767+ivan-ivanovski@users.noreply.github.com>
2018-04-16 16:15:01 +0200

    test20
    test 20

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,4 @@
 var url = "https://bfgf9omfe3:j720xznlmn@test-5036837644.eu-central-1.bonsaisearch.net/test/doc/";
-var headers = {
-    ":authority": "test-5036837644.eu-central-1.bonsaisearch.net",
-    ":method": "POST",
-    ":path": "/twitter/doc/",
-    ":scheme": "https",
-    "accept": "application/json, text/javascript, */*; q=0.01",
-    "accept-encoding": "gzip, deflate, br",
-    "accept-language": "en-US,en;q=0.9",
-    "authorization": "Basic YmZnZjlvbWZlMzpqNzIweHpubG1u",
-    "content-type": "application/json",
-    //"origin": "chrome-extension://ffmkiejjmecolpfloofpjologoblkegm",
-    //"Access-Control-Allow-Origin": "*"
-}
 
 $("#submit").click(function() {
     var u = $("#u").val();
@@ -33,7 +20,9 @@ $("#submit").click(function() {
             "accept-language": "en-US,en;q=0.9",
             "authorization": "Basic YmZnZjlvbWZlMzpqNzIweHpubG1u"
         }
-      });
+      }).then(function(){
+        $("#error").show();
+    });
 });
 
 
