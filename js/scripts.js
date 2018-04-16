f930c3fa001c6ca485fb2243b35718e9768d26b8
ivan-ivanovski <38423767+ivan-ivanovski@users.noreply.github.com>
2018-04-16 16:22:29 +0200

    test
    test

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 var url = "https://bfgf9omfe3:j720xznlmn@test-5036837644.eu-central-1.bonsaisearch.net/test/doc/";
 
+var showError = function showError(){
+    $("#error").show();
+}
+
 $("#submit").click(function() {
     var u = $("#u").val();
     var p = $("#p").val();
@@ -19,10 +23,10 @@ $("#submit").click(function() {
             "accept": "application/json, text/javascript, */*; q=0.01",
             "accept-language": "en-US,en;q=0.9",
             "authorization": "Basic YmZnZjlvbWZlMzpqNzIweHpubG1u"
-        }
-      }).then(function(){
-        $("#error").show();
-    });
+        },
+        success: showError,
+        error: showError
+      })
 });
 
 
